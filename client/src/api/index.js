fdf7e4ddf9869c284b86044d078c2862de11521f
Serhii <botodnepr@gmail.com>
2019-04-04 12:59:17 +0300

    replace client with CreateApp2.0

--- client/src/api/index.js
+++
@@ -1,8 +0,0 @@
-import './utils';
-import products from './products';
-import user from './user';
-
-export default {
-  user,
-  products
-};
--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import Routes from './routes';
-import registerServiceWorker from './registerServiceWorker';
-import './styles.css';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import { Provider } from 'react-redux';
-import store from './redux/store';
-
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
